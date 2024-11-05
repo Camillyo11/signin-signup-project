9b23e00b3174b8847bad272676a1a12ca25429d1
LucasAmerico0101 <lucas.silv4427@etec.sp.gov.br>
2024-11-04 22:04:46 -0300

    Adicionado tela de login

--- src/components/screen/input/styles.ts
+++ src/components/screen/input/styles.ts
@@ -1,2 +1,12 @@
-import styled from 'styled-components/native';​
-export const Container = styled.TextInput``;
+import styled from 'styled-components/native';
+
+import { View } from 'react-native';
+export const Container = styled(View)
+`background-color: #29292e;
+color: #f1f1f1;
+padding: 15px;
+marginTop: 30px;
+borderRadius: 7px;
+width: 300px;
+alignItems: center;
+;`
